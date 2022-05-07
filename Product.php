1cfc2f52a8710f4741e73765cb36cb0a2640f2fa
KHegedus1 <kico.kiki@gmail.com>
2022-05-07 14:31:14 +0200

    get i set metode za proizvode

---
+++ Product.php
@@ -0,0 +1,43 @@
+<?php
+//Koristenje get i set metoda na klasi proizvod
+
+
+class Product
+{
+    private $sku;
+    private $quantity;
+    private $name;
+    private $price;
+
+
+	public function setSku($sku){
+		$this->sku = $sku;
+	}
+    public function getSku(){
+		return $this->sku;
+	}
+
+
+	public function setQuantity($quantity){
+		$this->quantity = $quantity;
+	}
+    public function getQuantity(){
+		return $this->quantity;
+	}
+
+
+    public function setName($name){
+		$this->name = $name;
+	}
+	public function getName(){
+		return $this->name;
+	}
+
+	
+    public function setPrice($price){
+		$this->price = $price;
+	}
+	public function getPrice(){
+		return $this->price;
+	}
+} 

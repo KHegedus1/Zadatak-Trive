1f786004213f080d05dfd5c0068016dc2db114ab
KHegedus1 <kico.kiki@gmail.com>
2022-05-07 14:15:02 +0200

    Prirpema starta

---
+++ Start.php
@@ -0,0 +1,8 @@
+<?php
+//Pokretanje aplikacije,dohvacanje drugih dijelova aplikacije
+
+require_once 'Application.php';
+require_once 'Controller.php';
+require_once 'Item.php';
+
+new Application();
